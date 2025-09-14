090dee65db471b1c0dbb43068f20f268ab24b030
Yeonsuk Yoo <yysuk0302@gmail.com>
2025-09-14 21:48:54 +1000

    feat: create useAddressValidation custom hook

---
+++ src/hooks/use-address-validation.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+import { useLazyQuery } from '@apollo/client/react';
+import { toast } from 'sonner';
+
+import { ValidateAddressResponse } from '@/app/api/graphql-proxy/route';
+import { VALIDATE_ADDRESS } from '@/lib/graphql/query';
+import { ValidateAddressSchema } from '@/validation/validate-address';
+
+export function useAddressValidation() {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [runQuery, { loading, data, error }] =
+    useLazyQuery<ValidateAddressResponse['data']>(VALIDATE_ADDRESS);
+
+  const validateAddress = (variables: ValidateAddressSchema) => {
+    setErrorMessage(null);
+    runQuery({
+      variables
+    });
+  };
+
+  useEffect(() => {
+    if (error) {
+      setErrorMessage('Network error. Please try again later.');
+      return;
+    }
+
+    if (!data?.validateAddress) return;
+
+    const result = data.validateAddress;
+
+    if (!result.success) {
+      setErrorMessage(result.message);
+      toast.error(result.message);
+    } else {
+      setErrorMessage(null);
+      toast.success(result.message);
+    }
+  }, [data, error]);
+
+  return {
+    validateAddress,
+    loading,
+    error: errorMessage
+  };
+}
---
+++ src/lib/graphql/query.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export const VALIDATE_ADDRESS = gql`
+  query ValidateAddress($postcode: String!, $suburb: String!, $state: String!) {
+    validateAddress(postcode: $postcode, suburb: $suburb, state: $state) {
+      success
+      message
+    }
+  }
+`;

482f5439a804663113f39be99dcd25bafb2bd2de
Yeonsuk Yoo <yysuk0302@gmail.com>
2025-09-16 06:34:24 +1000

    fix: fix google map bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ApolloProvider } from '@apollo/client/react';
-import { LoadScript } from '@react-google-maps/api';
 
 import Header from '@/components/core/Header';
 import Map from '@/components/verifier/Map';
@@ -12,23 +11,19 @@ import { apolloClient } from '@/lib/apollo';
 export default function Home() {
   return (
     <ApolloProvider client={apolloClient}>
-      <LoadScript
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-      >
-        <AddressValidationProvider>
-          <Header />
-          <main className='mx-auto mt-6 max-w-2xl px-4'>
-            <h1 className='mb-6 text-lg font-bold sm:mb-8 sm:text-xl'>
-              Address Verification
-            </h1>
+      <AddressValidationProvider>
+        <Header />
+        <main className='mx-auto mt-6 max-w-2xl px-4'>
+          <h1 className='mb-6 text-lg font-bold sm:mb-8 sm:text-xl'>
+            Address Verification
+          </h1>
 
-            <div className='flex flex-col gap-4 pb-10 sm:flex-row'>
-              <VerifierForm />
-              <Map />
-            </div>
-          </main>
-        </AddressValidationProvider>
-      </LoadScript>
+          <div className='flex flex-col gap-4 pb-10 sm:flex-row'>
+            <VerifierForm />
+            <Map />
+          </div>
+        </main>
+      </AddressValidationProvider>
     </ApolloProvider>
   );
 }

5cac44ab10e1bb9d71eb7893faddb6d249950ae7
Yeonsuk Yoo <yysuk0302@gmail.com>
2025-09-16 03:26:10 +1000

    feat: create Map component for rendering Google Maps

---
+++ src/components/verifier/Map.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { GoogleMap, Marker } from '@react-google-maps/api';
+
+import { useAddressValidation } from '@/contexts';
+
+export default function Map() {
+  const { coordinates, successMessage } = useAddressValidation();
+
+  if (successMessage && coordinates === null) {
+    return (
+      <div className='flex h-[300px] w-full items-center justify-center rounded-lg border p-4 text-sm'>
+        <p className='text-center'>No coordinates found for this address 🫥</p>
+      </div>
+    );
+  }
+
+  if (coordinates === null) return null;
+
+  const containerStyle = { width: '100%', height: '300px' };
+  const center = {
+    lat: coordinates.latitude,
+    lng: coordinates.longitude
+  };
+
+  return (
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+      <Marker position={center} />
+    </GoogleMap>
+  );
+}

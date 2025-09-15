062d7244a1bfde1bbba097885572febf3f5534d6
Yeonsuk Yoo <yysuk0302@gmail.com>
2025-09-15 11:36:05 +1000

    feat: clear verifier form data in localStorage when logout

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -3,14 +3,20 @@
 import { logOut } from '@/server-actions/log-out';
 
 import Logo from '../svg/Logo';
+import { VERIFIER_FORM_KEY } from '../verifier/VerifierForm';
 
 export default function Header() {
+  const clearFormData = () => {
+    localStorage.removeItem(VERIFIER_FORM_KEY);
+  };
+
   return (
     <header className='sticky top-0 z-30 border-b bg-white px-4 py-2'>
       <div className='flex items-center justify-between'>
         <Logo className='w-32 sm:w-40' />
         <form
           action={async () => {
+            clearFormData();
             await logOut();
           }}
         >
